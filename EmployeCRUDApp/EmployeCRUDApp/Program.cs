57e5c4331215a4a5e71850c8e1868d813cfb872a
POKA TCHONENG Hermann <pokatchoneng@yahoo.fr>
2020-04-29 11:56:39 +0100

    part 64 AspNetCore Log Level Configuration

--- EmployeCRUDApp/EmployeCRUDApp/Program.cs
+++ EmployeCRUDApp/EmployeCRUDApp/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 
 namespace EmployeCRUDApp
 {
@@ -19,6 +20,13 @@ namespace EmployeCRUDApp
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostingContext, logging)=> {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                    logging.AddEventSourceLogger();
+                    logging.AddNLog();
+                })
                 .UseStartup<Startup>();
     }
 }
